d919a8e036f3bada2aafe25b38c421c2f5dfb6dd
Justus Schwabedal <jschwabedal@gmail.com>
2022-01-28 09:35:11 +0100

    Chg(header): split off channel classes

---
+++ edfpy/channel_difference.py
@@ -0,0 +1,43 @@
+class ChannelDifference:
+
+    def __init__(self, left, right):
+        left.check_compatible(right)
+        self.left = left
+        self.right = right
+        self.label = left.label-right.label
+
+    def check_compatible(self, other):
+        """check compatibility of left and right
+
+        note that compatibility is transitive. left vs right already checked in
+        `__init__`.
+        """
+        self.left.check_compatible(other)
+
+    @property
+    def type(self):
+        return self.label.type
+
+    @property
+    def sampling_rate(self):
+        return self.left.sampling_rate
+
+    @property
+    def physical_dimension(self):
+        return self.left.physical_dimension
+
+    def digital2physical(self, *args, **kwargs):
+        return self.left.digital2physical(*args, **kwargs) \
+            - self.right.digital2physical(*args, **kwargs)
+
+    def __sub__(self, other):
+        return ChannelDifference(self, other)
+
+    @property
+    def output_physical_dimension(self):
+        return self.left.output_physical_dimension
+
+    @output_physical_dimension.setter
+    def output_physical_dimension(self, v):
+        self.left.output_physical_dimension = v
+        self.right.output_physical_dimension = v
